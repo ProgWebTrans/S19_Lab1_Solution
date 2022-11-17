242d50db4958ab482a9936e2a0b07b20f8b2ba42
Thierry Giroux Veilleux <64013433+t-gv@users.noreply.github.com>
2022-11-16 19:05:02 -0500

    Initial commit

---
+++ PresseMots/PresseMots_Utility/WordCounter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace PresseMots_Utility
+{
+    public  class WordCounter
+    {
+        public  int Count(string input) {
+
+            string pattern = "[^\\w]";
+            var wordCount = Regex.Split(input ?? String.Empty, pattern, RegexOptions.IgnoreCase | RegexOptions.Multiline).Where(s => !string.IsNullOrWhiteSpace(s)).Count();
+            return wordCount;
+
+        }
+
+        public  int Count(IWordCountable wordCountable) {
+            return Count(wordCountable.Content);
+        }
+            
+            
+            
+
+    }
+}
---
+++ PresseMots/PresseMots_Web/Services/IStoryService.cs
@@ -0,0 +1,58 @@
+﻿using PresseMots_DataAccess.Context;
+using PresseMots_DataAccess.Services;
+using PresseMots_DataModels.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vereyon.Web;
+
+namespace PresseMots_Web.Services
+{
+    public interface IStoryService : IServiceBase<Story>
+    {
+        public List<Story> SearchByTagName(string tagName);
+    }
+    public class StoryService : ServiceBaseEF<Story>, IStoryService
+    {
+        private HtmlSanitizer sanitizer;
+
+        public StoryService(PresseMotsDbContext dbContext, HtmlSanitizer sanitizer) : base(dbContext)
+        {
+            this.sanitizer = sanitizer;
+        }
+
+        public List<Story> SearchByTagName(string tagName)
+        {
+            return _dbContext.Stories.Where(x => x.StoryTags.Any(y => y.Tag.Name.ToUpper() == tagName.ToUpper())).ToList();
+        }
+
+        public override Story Create(Story story) {
+
+            story.CreationTime = DateTime.Now;
+            story.PublishTime = null;
+            story.LastEditTime = null;
+            story.Draft = true;
+            story.Content = sanitizer.Sanitize(story.Content);
+            return base.Create(story);
+
+
+        }
+
+        public override void Edit(Story story)
+        {
+            
+            story.LastEditTime = DateTime.Now;
+            story.Content = sanitizer.Sanitize(story.Content);
+
+            base.Edit(story);
+
+
+        }
+
+
+
+    }
+
+
+}
